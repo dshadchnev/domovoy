107e17d414e18187fd1f3d0fccd7f7f7764af2aa
Anna <a.romancova@profit-ug.ru>
2026-08-25 10:59:21 +0300

    Remove Unused directory

--- Unused/Domovoy.Auth.Service/Presentation/Contracts/DeviceCredentialResponse.cs
+++
@@ -1,6 +0,0 @@
-namespace Domovoy.Auth.Service.Presentation.Contracts;
-
-public record DeviceCredentialResponse(
-    string NetworkDeviceId,
-    string Secret,
-    string Message = "Store the secret securely. It won't be shown again.");
--- Unused/Domovoy.Auth.Service/Presentation/Contracts/RefreshTokenRequest.cs
+++
@@ -1,3 +0,0 @@
-namespace Domovoy.Auth.Service.Presentation.Contracts;
-
-public record RefreshTokenRequest(string RefreshToken);
